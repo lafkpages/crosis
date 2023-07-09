55ac644824b4ee1c73f1e536d228d93674d4b26f
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2023-07-09 22:50:36 +0200

    🗿Added comment

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,6 +1,7 @@
 import { Crosis } from "..";
 import { replit } from "../lib/adapters";
 
+// This is a basic test
 const crosis = new Crosis({
   adapter: replit({
     replId: process.env.REPL_ID,
