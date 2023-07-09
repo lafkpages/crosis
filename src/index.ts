7a26bc06ee1720eba3029059d6ba32b8a7555fc6
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2023-07-09 22:05:20 +0200

    🗿Finish connect function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { WebSocket } from 'ws';
-import type { CrosisOptions, Adapter } from "./lib/types"
+import { WebSocket } from "ws";
+import type { CrosisOptions, Adapter } from "./lib/types";
 
 const defaultOptions: CrosisOptions = {
   autoConnect: true,
@@ -7,26 +7,33 @@ const defaultOptions: CrosisOptions = {
 
 class Crosis {
   private url: string | null;
-  private adapter: Adapter;
+  private adapter: Adapter | null;
   private ws: WebSocket;
 
   constructor(options: CrosisOptions) {
     options = {
       ...defaultOptions,
-      ...options
+      ...options,
     };
 
-    this.adapter = options.adapter;
+    this.adapter = options.adapter || null;
 
     this.url = options.url || undefined;
     this.ws = null;
   }
 
   async connect() {
-    const adapterResult = this.adapter
-      ? await this.adapter()
-      : null;
+    const adapterResult = this.adapter ? await this.adapter() : null;
 
-    
+    if (adapterResult?.url) {
+      this.url = adapterResult.url;
+    }
+
+    this.ws = new WebSocket(this.url);
+
+    // Wait for the WebSocket to be ready
+    await new Promise((resolve) => {
+      this.ws.onopen = resolve;
+    });
   }
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,10 +2,10 @@ export interface CrosisOptions {
   adapter?: Adapter;
   autoConnect?: boolean;
   url?: string;
-};
+}
 
-export type Adapter = () => AdapterResult;
+export type Adapter = () => Promise<AdapterResult>;
 
 export interface AdapterResult {
   url?: string;
-};
+}
