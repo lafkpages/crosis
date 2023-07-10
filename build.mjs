53238d77fb29404810772c7fa7b239feeacca076
lafkpages <soy.lafk+gh@gmail.com>
2023-07-10 20:25:35 +0200

    🗿Remove build hacky trick

--- build.mjs
+++ build.mjs
@@ -10,35 +10,19 @@ const sharedConfig = {
     "./src/lib/adapters/replit.ts",
   ], // TODO: use glob
   logLevel: "info",
-  minify: false,
+  minify: true,
   sourcemap: true,
+  outdir: "dist",
+  target: ["esnext", "node12.22.0"],
 };
 
 await build({
   ...sharedConfig,
   format: "esm",
-  outdir: "dist",
-  target: ["esnext", "node12.22.0"],
-  banner: {
-    js: "import*as _r_p from'@replit/protocol';const protocol=_r_p.default.api;import{WebSocket}from'ws'/*",
-
-    /* 
-      This is a really hacky trick.
-
-      Because @replit/protocol types are broken, the imports
-      used in src/lib/crosis.ts and src/lib/channels.ts aren't
-      actually valid, but they make the types work. So here,
-      we comment out those invalid imports and replace them
-      with ones that actually work.
-    */
-  },
-  legalComments: "inline",
 });
 
 await build({
   ...sharedConfig,
   format: "cjs",
-  outdir: "dist",
   outExtension: { ".js": ".cjs" },
-  target: ["esnext", "node12.22.0"],
 });
