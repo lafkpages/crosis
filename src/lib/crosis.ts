acd23d3ae82b0381e603705110e6c59521cbba0b
lafkpages <soy.lafk+gh@gmail.com>
2023-09-10 08:36:28 +0200

    🗿 Fix wsReadyState to use `??` instead of `||`

--- src/lib/crosis.ts
+++ src/lib/crosis.ts
@@ -107,9 +107,7 @@ class Crosis extends EventEmitter {
    * Returns the WebSocket ready state.
    */
   get wsReadyState() {
-    // TODO: if it's 0, the or will trigger
-
-    return this.ws?.readyState || WebSocket.CLOSED;
+    return this.ws?.readyState ?? WebSocket.CLOSED;
   }
 
   /**
